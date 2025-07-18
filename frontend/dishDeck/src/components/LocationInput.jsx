2997735a29ef2fa4d7c85e3cb4391a17d2c9ce62
Arsh <pawar.arsh1333@gmail.com>
2025-07-18 20:05:09 +0530

    Location Input file

--- frontend/dishDeck/src/components/LocationInput.jsx
+++ frontend/dishDeck/src/components/LocationInput.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function LocationInput() {
   const [query, setQuery] = useState("");
@@ -10,7 +11,63 @@ function LocationInput() {
       setResults([]);
       return;
     }
+
+    const fetchSuggestions = async (e) => {
+      try {
+        const res = await axios.fetch(
+          `https://photon.komoot.io/api/?q=${encodeURIComponent(
+            query
+          )}&limit=5`,
+          { signal: controller.signal }
+        );
+        const data = res.json();
+        setResults(data);
+      } catch (error) {
+        if (err.name !== "AbortError") {
+          console.error("Error fetching locations:", err);
+        }
+        console.log(error);
+      }
+    };
+    const delayDebounce = setTimeout(fetchSuggestions, 300);
+    return () => {
+      clearTimeout(delayDebounce);
+      controller.abort();
+    };
   }, [query]);
+  const handleSelect = (place) => {
+    setSelected(place.properties.name);
+    setQuery(place.properties.name);
+    setResults([]);
+    onSelect(place); // pass full place object to parent
+  };
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        className="border p-2 w-full rounded"
+        placeholder="Search for a location"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {results.length > 0 && (
+        <ul className="absolute bg-white border rounded shadow-md mt-1 z-10 w-full">
+          {results.map((place, i) => (
+            <li
+              key={i}
+              onClick={() => handleSelect(place)}
+              className="p-2 hover:bg-gray-100 cursor-pointer"
+            >
+              {place.properties.name},{" "}
+              {place.properties.city ||
+                place.properties.state ||
+                place.properties.country}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default LocationInput;
