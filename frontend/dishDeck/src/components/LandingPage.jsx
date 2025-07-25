c67570ce10ecf327aa29b07931eb95a9c16d0536
Arsh <pawar.arsh1333@gmail.com>
2025-07-26 00:28:38 +0530

    logo added

--- frontend/dishDeck/src/components/LandingPage.jsx
+++ frontend/dishDeck/src/components/LandingPage.jsx
@@ -7,9 +7,13 @@ function LandingPage({ user, onLogin }) {
     <>
       <div className="min-h-screen font-sec bg-gray-50  flex flex-col items-center justify-center px-4 py-8 sm:px-6 lg:px-8">
         <div className="bg-white p-6 sm:p-8 rounded-2xl shadow-xl max-w-xl w-full text-center opacity-0 translate-y-5 animate-fadeInUp">
-          <h1 className="text-4xl sm:text-5xl font-extrabold text-gray-800 mb-4 tracking-tight">
-            {" "}
+          <h1 className="flex items-center gap-4 text-4xl sm:text-5xl font-extrabold text-gray-800 mb-4 ml-[120px] tracking-tight">
             DishDeck
+            <img
+              src="/public/favicon.ico"
+              className="h-[80px] w-[80px]"
+              alt="DishDeck Logo"
+            />
           </h1>
 
           <p className="text-base sm:text-lg text-gray-600 leading-relaxed mb-6">
